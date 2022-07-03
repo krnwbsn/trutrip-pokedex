9b94acd6422ef674d43d943cf252bb70f736198e
krnwbsn <krinawibisana@gmail.com>
2022-07-03 07:27:55 +0700

    [ADD] document and offline files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa');
+const path = require('path');
 
-module.exports = nextConfig
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+  webpack(config, _options) {
+    config.resolve.alias['@pages'] = path.join(__dirname, 'pages');
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
+    config.resolve.alias['@interfaces'] = path.join(__dirname, 'interfaces');
+    config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+    config.resolve.alias['@containers'] = path.join(__dirname, 'containers');
+
+    return config;
+  },
+});
+
+module.exports = nextConfig;
